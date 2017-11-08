68de2e4a5c1fc1b5e807f2dedeb54875f774aca8
John Duckmanton <john Duckmanton>
2017-11-08 14:29:20 +0000

    Added README.md

--- HelloWorldLogicApp/Deploy-AzureResourceGroup.ps1
+++ HelloWorldLogicApp/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'LogicApps',
+    [string] $ResourceGroupName = 'HelloWorldLogicApp',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
